d4a0bcf4ed2573fd031e3474283338c6799b9b14
Augustus Mayo <gusmayo@gmail.com>
2015-08-28 01:14:17 -0400

    Initial icon work

--- js/background/background.js
+++ js/background/background.js
@@ -1,5 +1,12 @@
 var isInitialized = false;
 
+// On initialization show the appropriate icon based on state
+var state = localStorage.getItem("justice.run") !== "false";
+
+// Set the icon based on the state
+chrome.browserAction.setIcon({path: "../../img/fpsIcon-38" + (state ? "" : "-disabled") + ".png"});
+
+// Create the event listener for events from the tab and options
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 
   // If the browser action requested for Justice to initialize
@@ -14,12 +21,16 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
   if (message.type === "justice.start") {
     sendCommandToContent(message.type);
     localStorage.setItem("justice.run", true);
+    // Set the icon based on the state
+    chrome.browserAction.setIcon({path: "../../img/fpsIcon-38.png"});
   }
 
   // If the browser action requested for Justice to stop
   if (message.type === "justice.stop") {
     sendCommandToContent(message.type);
     localStorage.setItem("justice.run", false);
+    // Set the icon based on the state
+    chrome.browserAction.setIcon({path: "../../img/fpsIcon-38-disabled.png"});
   }
 
   // If the content page sent an fps update
